2fa6806bc18dc0a0238510b189efe3c6e9178d10
Samuel Tseng <samueltseng@icloud.com>
2020-04-17 11:12:45 +0200

    Update pdf_extractor: Use tika server instead

--- app/pdf_extractor.py
+++ app/pdf_extractor.py
@@ -1,54 +1,21 @@
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO, BytesIO
-# from urllib.request import urlopen
+import re
 import requests
-
-def extract_pdf_content(file):
-    rsrcmgr = PDFResourceManager()
-    codec = 'utf-8'
-    outfp = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr=rsrcmgr, outfp=outfp, codec=codec, laparams=laparams)
-    with open(file, 'rb') as fp:
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        password = None
-        maxpages = 0
-        caching = True
-        pagenos = set()
-        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
-                                      check_extractable=True):
-            interpreter.process_page(page)
-    mystr = outfp.getvalue()
-    device.close()
-    outfp.close()
-    return mystr
+import tika
+from tika import parser
 
 
-def extract_pdf_content_url(url):
-    rsrcmgr = PDFResourceManager()
-    codec = 'utf-8'
-    outfp = StringIO()
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr=rsrcmgr, outfp=outfp, codec=codec, laparams=laparams)
-    f = requests.get(url, stream=True)
-    fp = BytesIO(f)
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-    for page in PDFPage.get_pages(fp,
-                                  pagenos,
-                                  maxpages=maxpages,
-                                  password=password,
-                                  caching=caching,
-                                  check_extractable=True):
-        interpreter.process_page(page)
-    fp.close()
-    mystr = outfp.getvalue()
-    device.close()
-    outfp.close()
-    return mystr
+def parse_from_file(file):
+    content_parsed = tika.parser.from_file(filename=file)
+    text = content_parsed['content']
+    content = text.encode("ascii", "ignore").decode("utf-8")
+    output = re.sub(r"[\n\r\s]+", " ", content)
+    return output
+
+
+def parse_from_url(url):
+    pdf = requests.get(url, stream=True)
+    content_parsed = tika.parser.from_buffer(pdf)
+    text = content_parsed['content']
+    content = text.encode("ascii", "ignore").decode("utf-8")
+    output = re.sub(r"[\n\r\s]+", " ", content)
+    return output
